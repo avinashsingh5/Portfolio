fb4ad3f5c80cdd6966cbc90e89a4dea08b29ca40
Avinash Kumar <avinashkumar90202@gmail.com>
2025-08-11 23:46:29 +0530

    update

--- src/hooks/useScrollFloat.ts
+++ src/hooks/useScrollFloat.ts
@@ -2,12 +2,12 @@ import { useRef } from 'react'
 import { useScroll, useTransform, MotionValue, useReducedMotion } from 'framer-motion'
 
 type ScrollFloat = {
-  ref: React.RefObject<HTMLElement>
+  ref: React.RefObject<HTMLElement | null>
   y: MotionValue<number>
 }
 
 export function useScrollFloat(amplitude: number = 24): ScrollFloat {
-  const ref = useRef<HTMLElement>(null)
+  const ref = useRef<HTMLElement | null>(null)
   const prefersReduced = useReducedMotion()
   const { scrollYProgress } = useScroll({ target: ref, offset: ['start end', 'end start'] })
   const y = useTransform(scrollYProgress, [0, 1], prefersReduced ? [0, 0] : [amplitude, -amplitude])
